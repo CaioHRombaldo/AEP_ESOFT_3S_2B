70b15af8b941ef513dd345765a93a0743cc38553
Caio Lindo <caiorombaldo@alunos.unicesumar.edu.br>
2022-06-14 18:16:13 -0300

    Caio-001 Adição do sistema de leitura dos dados fixos e editaveis do mock de clientes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <locale.h>
 
 
 struct endereco
@@ -23,39 +24,152 @@ struct servico
 
 struct usuario_cliente
 {
-    char nome[256];
-    char email[256];
-    char senha[256];
-	int cpf[256];
+    char nome[34];
+    char email[50];
+    char senha[32];
+	char cpf[11];
     int avaliacaotot;
     int avaliacaonum;
     char numero_celular[256];
-    char sexo;
+    char sexo[5];
     
-    struct endereco endereco_cliente[256];
+    struct endereco endereco_cliente[32];
 };
 
 struct usuario_prestador
 {
-    char nome[256];
-    char email[256];
-    char senha[256];
+    char nome[34];
+    char email[50];
+    char senha[32];
     char numero_celular[256];
     char profissao[256];
-    int cpf[256];
-    char sexo;
+    char cpf[11];
+    char sexo[5];
     int avaliacaotot;
     int avaliacaonum;
 
-    struct servico servicos_prestador[256];
-    struct endereco endereco_prestador[256];
+    struct servico servicos_prestador[32];
+    struct endereco endereco_prestador[32];
     
 
 };
 
-int main(){
+// Declaração de Variaveis Globais:
+struct usuario_cliente clientes[256];
+int totClientes = 0;
 
-    printf("Hello, World!");
+struct usuario_prestador prestadores[256];
+int totPrestadores = 0;
+
+char cpf_temp[11];
+
+// Fim Declaração de Variaveis Globais
+
+int BuscaClienteCPF(char* cpf){
+    int count = 0;
+    for(count = 0; count<totClientes; count++){
+        if(strcmp(clientes[count].cpf, cpf) == 0){
+            return count;
+        }
+    }
+
+    return -1;
+}
+
+void LeCampo(char* campo, int ini, int fim, char* linha){
+    // Separa o campo
+    int c = 0;
+    for(c = ini; c <= fim; c++){
+        campo[c-ini] = linha[c];
+    }
+
+    // Remove os caracteres vazios
+    int tam = fim - ini;
+
+    while(campo[tam] == '=' || campo[tam] == ' '){
+        campo[tam] = '\0';
+        tam--;
+    }
+
+}
+
+void CargaDeDadosEditaveisClientes(){
+    FILE *arq;
+    char linha[78];
+    char *result;
+    int count = 0;
+    int cliente_id;
+
+    arq = fopen("Dataclienteedit.txt","r");
+    
+    if(arq == NULL) {
+        printf("Problema ao abrir o arquivo editavel.\n");
+        return;
+
+    } else {
+        while(!feof(arq)){
+
+            result = fgets(linha, 78, arq);
+
+            if (result){
+                LeCampo(cpf_temp, 65, 75, linha);
+                cliente_id = BuscaClienteCPF(cpf_temp);
+                
+                if (cliente_id != -1){
+                    LeCampo(clientes[cliente_id].senha, 0, 32, linha);
+
+                }
+
+            }
+
+        }
+
+    }
+    
+}
+
+void CargaDeDadosClientes(){
+    FILE *arq;
+    char linha[102];
+    char *result;
+
+    arq = fopen("Dataclientes.txt","r");
+    
+    if(arq == NULL) {
+        printf("Problema ao abrir o arquivo.\n");
+        return;
+
+    } else {
+        while(!feof(arq)){
+
+            result = fgets(linha, 102, arq);
+
+            if (result){
+                LeCampo(clientes[totClientes].nome, 0, 32, linha);
+                LeCampo(clientes[totClientes].email, 34, 83, linha);
+                LeCampo(clientes[totClientes].sexo, 84, 88, linha);
+                LeCampo(clientes[totClientes].cpf, 89, 99, linha);
+            
+            }
+            totClientes++;
+        }
+        CargaDeDadosEditaveisClientes();
+
+    }
+}
+
+
+int main(){
+    setlocale (LC_ALL, "");
+    setlocale (LC_CTYPE, "pt_BR.UTF-8");
+    
+    printf("Compilei!!\n");
+    CargaDeDadosClientes();
+    printf("nome: %s\n", clientes[1].nome);
+    printf("email: %s\n", clientes[1].email);
+    printf("cpf: %s\n", clientes[1].cpf);
+    printf("sexo: %s\n", clientes[1].sexo);
+    printf("SENHA: %s\n", clientes[1].senha);
 
     return 0;
 }
